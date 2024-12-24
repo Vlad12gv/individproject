ca76152957040c1e66dcc0cfab7a2f49eb2b117c
Vlad12gv <gunasanvladislav@gmail.com>
2024-12-24 09:10:11 +0000

    Сделал

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
+import os
 import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
+from dotenv import find_dotenv, load_dotenv
 
-TOKEN = '7887208020:AAG4pV1eZZg3I9L7rB9P2CjapgGhaQxGdic'
+load_dotenv(find_dotenv())
+TOKEN = os.getenv("TOKEN")
+
 bot = Bot(TOKEN)
 dp = Dispatcher()
 
+
 @dp.message(CommandStart())
 async def start_cmd(message: types.Message):
-    await message.answer("Alen хач, любит тасю!")
+    await message.answer("Была команда старт")
 
+
+# @dp.message()
+# async def slon(message: types.Message):
+#     if message.text == "Утро":
+#         await message.answer("Пьем кофэ")
+
+
+# @dp.message()
+# async def slon(message: types.Message):
+#     if message.text == "День":
+#         await message.answer("Ем супчик")
 
 
 @dp.message()
-async def echo(message: types.Message):
-    await message.answer("Сам ты осел глупый!")
+async def slon(message: types.Message):
+    if message.text == "Вечер":
+        await message.answer("Пью чай")
+    else:
+        await message.answer(message.text)
+
+
+# @dp.message()
+# async def echo(message: types.Message):
+#     await message.answer("Напиши что то умное!")
 
 
 async def main():
